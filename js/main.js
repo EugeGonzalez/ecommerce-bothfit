e39b61cb5395fffe65ff53225995f142901ebda0
Euge Gonzalez <gonzalezm.euge97@gmail.com>
2022-10-16 22:03:23 -0300

    arreglo nav

--- js/main.js
+++ js/main.js
@@ -139,7 +139,7 @@ function formulario_c(){
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="./index.html#contenedor-productos">Productos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./index.html">Productos</a></li>
                     <li class="nav-item"><a class="nav-link" href="#!">Nosotros</a></li>
                     <li class="nav-item" id="mostrar_carrito"><a class="bi bi-cart3 carrito"></a></li>
                     
